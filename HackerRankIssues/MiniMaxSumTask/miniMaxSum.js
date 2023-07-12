bae3f6fac03a79fd82761c0da21478f8f9ee592e
Anahit.Nalbandyan <anahitgrigoryanr@gmail.com>
2023-07-12 10:57:00 +0200

    My new inputing code

--- HackerRankIssues/MiniMaxSumTask/miniMaxSum.js
+++ HackerRankIssues/MiniMaxSumTask/miniMaxSum.js
@@ -27,8 +27,10 @@ function readLine() {
  */
 
 function miniMaxSum(arr) {
-    // Write your code here
-
+    let sum = arr.reduce((a, b) => a + b);
+    let maxVal = Math.max(...arr);
+    let minVal = Math.min(...arr);
+    console.log((sum - maxVal) + ' ' + (sum - minVal));
 }
 
 function main() {
